7c7a52b6ef6dfc50b1caf33efa03594b903ec899
Fernando Cordero <fcordero@acid.cl>
2021-04-30 07:55:57 -0400

    class-validator and mapped-types

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,10 +1,12 @@
+import { IsString, IsNumber, IsPositive } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
 export class CreateProductDto {
+  @IsString()
   readonly sku: string;
   readonly name: string;
+  readonly url: string;
+  @IsNumber()
+  @IsPositive()
   readonly price: number;
 }
-export class UpdateProductDto {
-  readonly sku?: string;
-  readonly name?: string;
-  readonly price?: number;
-}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}

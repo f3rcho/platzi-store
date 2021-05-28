f15fa372048f01fe0a283f594507452d022958dd
Fernando Cordero <fernando.cordero@externos-cl.cencosud.com>
2021-05-28 14:26:52 -0400

    Revert "Typo fixed"
    This reverts commit e1d771df4c39f21d000d4512f23e886f99907e1d.

--- src/users/controllers/orders.controller.spec.ts
+++ src/users/controllers/orders.controller.spec.ts
@@ -1,47 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersController } from './orders.controller';
-import { OrdersService } from '../services/orders.service';
 
 describe('OrdersController', () => {
   let controller: OrdersController;
 
-  const mockOrdersService = {
-    create: jest.fn((dto) => {
-      return {
-        id: 'x,y,z',
-        ...dto,
-      };
-    }),
-  };
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrdersController],
-      providers: [OrdersService],
-    })
-      .overrideProvider(OrdersService)
-      .useValue(mockOrdersService)
-      .compile();
+    }).compile();
 
     controller = module.get<OrdersController>(OrdersController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
-  });
-
-  it('Should create an order', () => {
-    expect(
-      controller.createOrder({
-        customer: '1234',
-        date: new Date(),
-        products: ['prodA', 'prodB'],
-      }),
-    ).toEqual({
-      id: 'x,y,zw',
-      customer: '1234',
-      date: new Date(),
-      products: ['prodA', 'prodB'],
-    });
   });
 });

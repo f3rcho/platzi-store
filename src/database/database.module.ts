7b270569e168731b97d6fa4329d5026c83d61c6e
Fernando Cordero <fernando.cordero@externos-cl.cencosud.com>
2021-06-14 12:37:44 -0400

    Installing and setting up TypeORM module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,10 +1,27 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { Client } from 'pg';
-import config from 'src/config/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from '../config/config';
 
 @Global()
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { dbName, user, port, host, password } = configService.postgres;
+        return {
+          type: 'postgres',
+          database: dbName,
+          username: user,
+          password,
+          port,
+          host,
+        };
+      },
+    }),
+  ],
   providers: [
     {
       provide: 'PG',
@@ -29,6 +46,6 @@ import config from 'src/config/config';
       inject: [config.KEY],
     },
   ],
-  exports: ['PG'],
+  exports: ['PG', TypeOrmModule],
 })
 export class DatabaseModule {}

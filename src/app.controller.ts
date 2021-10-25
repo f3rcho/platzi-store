beaa63a38b9bc3e2023a5357ae9dfc9a2bf5c760
Fernando Cordero <fernando.cordero@externos-cl.cencosud.com>
2021-10-25 15:54:45 -0300

    Back up

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  private readonly logger = new Logger(AppController.name);
 
   @Get()
   getHello(): string {
+    this.logger.log(`Testing logger in getHello`);
     return this.appService.getHello();
   }
   @Get('new')
